c8096ee2b667faf27d94c4735bb3beb54549526a
betulbasan <63635709+betulbasan@users.noreply.github.com>
2022-11-22 08:29:12 +0300

    create to-do-list
    edited to-do-list homework

---
+++ javascript/hw3/javascript.js
@@ -0,0 +1,73 @@
+/*function newElement(){
+    let task = document.querySelector("#task")
+    let ulDOM = document.querySelector('#list')
+    let liDOM = document.createElement('li')
+    var BUTTON_REMOVE = document.createElement('span');
+    BUTTON_REMOVE.textContent="X";
+    BUTTON_REMOVE.className = "delete-button";
+    BUTTON_REMOVE.addEventListener('click',() => {
+        liDOM.remove();
+    });
+    if(task.value && task.value !=" ")
+    {
+       ulDOM.appendChild(liDOM).innerHTML = task.value;
+       liDOM.appendChild(BUTTON_REMOVE)
+       task.value ="";
+    }else{
+    }
+}*/
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.querySelector('#task').value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.querySelector('#list').appendChild(li);
+  }
+  document.querySelector('#task').value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
